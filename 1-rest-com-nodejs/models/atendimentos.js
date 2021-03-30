3fa5984aff136d2127aab96c98b07109e1a35fe4
Pablo <pablowoina2205@gmail.com>
2021-03-30 17:32:54 -0300

    finalizando nossa API

--- 1-rest-com-nodejs/models/atendimentos.js
+++ 1-rest-com-nodejs/models/atendimentos.js
@@ -3,6 +3,7 @@ const moment = require("moment");
 const conexao = require("../infraestrutura/conexao");
 
 class Atendimento {
+
   adiciona(atendimento, res) {
     const dataCriacao = moment().format("YYYY-MM-DD");
     const data = moment(atendimento.data, "DD/MM/YYYY").format(
@@ -40,10 +41,66 @@ class Atendimento {
         if (erro) {
           res.stauts(400).json(erro);
         } else {
-          res.status(201).json(resultados);
+          res.status(201).json(atendimento);
         }
       });
     }
+  }
+
+  lista(res) {
+    const sql = 'SELECT * FROM Atendimentos'
+
+    conexao.query(sql, (erro, resultados) => {
+      if(erro){
+        res.status(400).json(erro)
+      }else{
+        res.status(200).json(resultados)
+      }
+    })
+  }
+
+  buscaPorId(id, res){
+
+    const sql = `SELECT * FROM Atendimentos WHERE id = ${id}`
+
+    conexao.query(sql, (erro, resultados) => {
+      const atendimento = resultados[0]
+      if(erro){
+        res.status(400).json(erro)
+      }else{
+        res.status(200).json(atendimento)
+      }
+    })
+  }
+
+  altera(id, valores, res){
+    if(valores.data){
+      valores.data = moment(valores.data, "DD/MM/YYYY").format(
+        "YYYY-MM-DD HH:MM:SS"
+      )
+    }
+
+    const sql = 'UPDATE Atendimentos SET ? WHERE id = ?'
+  
+    conexao.query(sql, [valores, id], (erro, resultados) => {
+      if(erro){
+        res.status(400).json(erro)
+      }else{
+        res.status(200).json({...valores, id})
+      }
+    })
+  }
+
+  deleta(id, res){
+    const sql = 'DELETE FROM Atendimentos WHERE id = ?'
+
+    conexao.query(sql, id, (erro, resultados) => {
+      if(erro){
+        res.status(400).json(erro)
+      }else{
+        res.status(200).json({id})
+      }
+    })
   }
 }
 
