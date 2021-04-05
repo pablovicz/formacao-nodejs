4d9d8ab1880d367d7d6e0ac5832ba539eda01468
Pablo <pablowoina2205@gmail.com>
2021-04-05 16:39:39 -0300

    initial commit

---
+++ 3-rest-padronizado-e-escalavel/api/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('config');
+
+
+const app = express();
+app.use(bodyParser.json())
+
+app.listen(config.get('api.porta'), () => {
+  console.log('Serving is running')
+})
