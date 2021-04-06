00ef233fbbe3453109c284d48c7ff0367c0935af
Pablo <pablowoina2205@gmail.com>
2021-04-06 14:11:36 -0300

    Iniciando a API rest

--- 3-rest-padronizado-e-escalavel/api/index.js
+++ 3-rest-padronizado-e-escalavel/api/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const config = require('config');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const config = require('config')
 
-
-const app = express();
 app.use(bodyParser.json())
 
-app.listen(config.get('api.porta'), () => {
-  console.log('Serving is running')
-})
+const roteador = require('./rotas/fornecedores')
+app.use('/api/fornecedores', roteador)
+
+app.listen(config.get('api.porta'), () => console.log('A API está funcionando!'))
