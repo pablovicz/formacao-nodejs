14c9c468af16d25f87306d87034db7df60d0a02a
Pablo <pablowoina2205@gmail.com>
2021-04-09 08:53:09 -0300

    CRUD com Sequelize

--- 5-orm-com-nodejs/api/controllers/PessoaController.js
+++ 5-orm-com-nodejs/api/controllers/PessoaController.js
@@ -10,7 +10,56 @@ class PessoaController {
       return res.status(500).json(error.message);
     }
   }
-}
 
+  static async pegaPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      const pessoa = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+
+      return res.status(200).json(pessoa);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async criaPessoa(req, res) {
+    const pessoa = req.body;
+
+    try {
+      const novaPessoaCriada = await database.Pessoas.create(pessoa);
+
+      return res.status(200).json(novaPessoaCriada);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async atualizaPessoa(req, res) {
+    const novasInfos = req.body;
+    const { id } = req.params;
+    try {
+        await database.Pessoas.update(novasInfos, {where: {id : id}})
+        const pessoaAtualizada = await database.Pessoas.findOne({where: {id : Number(id)}})
+
+        return res.status(200).json(pessoaAtualizada);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+
+  static async deletaPessoa(req, res){
+    const { id } = req.params
+
+    try {
+      await database.Pessoas.destroy({where: {id: Number(id)}})
+      return res.status(200).json({message: `Registro com id ${id} foi deletado com sucesso.`})
+    }catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+}
 
 module.exports = PessoaController;
--- 5-orm-com-nodejs/api/routes/pessoasRoute.js
+++ 5-orm-com-nodejs/api/routes/pessoasRoute.js
@@ -4,6 +4,10 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoas/:id', PessoaController.pegaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.delete('/pessoas/:id', PessoaController.deletaPessoa)
 
 module.exports = router
 
